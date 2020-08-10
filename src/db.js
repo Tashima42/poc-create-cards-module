aad37ee791f903f1d975bea7dcc7bb5a1597264b
Tashima42 <tashima42@pop-os.localdomain>
2020-08-10 16:56:21 -0300

    refactor(db.js): alterado o uso de promises para async/await
    Para evitar a legibilidade do codigo no futuro, o uso de promises foi substituido pelo uso de
    funcoes async/await
    
    fix: #3

--- src/db.js
+++ src/db.js
@@ -1,38 +1,56 @@
-const koaServer = require('./server')
 const mongoose = require('mongoose')
-const { cardRequestObject } = require('./server')
 const Schema = mongoose.Schema
+const server = require('./server')
 
+const dbConfig = {
+    url: 'mongodb://localhost:27017/cards',
+    user: '',
+    password: '',
+    options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+}
 
 //Conectar o mongoose ao mongo
-    mongoose.connect('mongodb://localhost:27017/cards', { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => console.log('Connected to MongoDB!'))
-        .catch(err => console.error(`Error connecting to MongoDB: ${err}`))
+const connectDb = async () => {
+    try {
+        await mongoose.connect(dbConfig.url, dbConfig.options)
+
+        console.info("Connected to MongoDB")
+    } catch (err) {
+        console.error(`Error connecting to MongoDB: ${err}`)
+        process.exit(1)
+    }
+}
+
+connectDb()
 
 //Esquema de card para o mongoose
-    const cardSchema = new Schema({
-        front: {
-            type: String,
-            require: true
-        },
-        back: {
-            type: String,
-            require: true
-        }
-    })
+const cardSchema = new Schema({
+    front: {
+        type: String,
+        require: true
+    },
+    back: {
+        type: String,
+        require: true
+    }
+})
 
 
 //Definindo um modelo usando o esquema do card
 const cardModel = mongoose.model('mycards', cardSchema)
 
 
-const createNewCard = (cardObject) => {
-    const cardJSON = JSON.stringify(cardObject)
-    new cardModel(cardObject)
-        .save()
-        .then(() => console.log(`Create card ${cardObject.front}}`))
-        .catch(err => console.error(`Error creating card: ${err}`))
-    
+const createNewCard = async (cardObject) => {
+    try {
+        await new cardModel(cardObject).save()
+        console.info(`Create card ${cardObject.front}`)
+    } catch (err) {
+        console.error(`Error creating card: ${err}`)
+    }
+
 }
 
 exports.createNewCard = createNewCard
