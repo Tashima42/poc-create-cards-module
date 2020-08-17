7b4907d924802299684753c4b2e9fd35ae7b9606
Tashima42 <tashima42@pop-os.localdomain>
2020-08-17 10:44:02 -0300

    feat: adicionado componente "criar baralho"
    Um componente de botao para criar um baralho foi adicionado e o componenete HelloWorld removido
    
    re: #24

--- frontend/poc-front-flashcards/src/components/CriarBaralho.vue
+++ frontend/poc-front-flashcards/src/components/CriarBaralho.vue
@@ -9,15 +9,29 @@ export default {};
 </script>
 
 <style lang="scss">
+@font-face {
+  font-family: "Roboto Condensed";
+  src: local("Roboto Condensed"),
+    url("https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap");
+}
+
 .btn-criar {
-  font-family: Roboto Condensed;
+  font-family: "Roboto Condensed", sans-serif;
   font-style: normal;
   font-weight: normal;
-  font-size: 36px;
+  font-size: 1.7em;
   line-height: 42px;
   text-align: center;
 
+  width: 30vw;
+  height: 7vh;
+
   background: #1f38d1;
   color: #e8e8e8;
+
+  box-shadow: 0px 12px 17px rgba(0, 0, 0, 0.14),
+    0px 5px 22px rgba(0, 0, 0, 0.12), 0px 7px 8px rgba(0, 0, 0, 0.2);
+  border: none;
+  border-radius: 10px;
 }
 </style>
