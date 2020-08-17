22a398cffaf9d50b3c6d0e7377b8b28396aef4d6
Tashima42 <tashima42@pop-os.localdomain>
2020-08-17 11:10:45 -0300

    feat: adicionado componente de visualizacao inicial do baralho
    A visualizacao inicial do baralho, que posteriormente fara parte de outro componenente, foi
    adicionada
    
    re: #24

--- frontend/poc-front-flashcards/src/App.vue
+++ frontend/poc-front-flashcards/src/App.vue
@@ -1,21 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
     <CriarBaralho />
+    <BaralhoInicial corBg="#8130D1" />
   </div>
 </template>
 
 <script>
 import CriarBaralho from "./components/CriarBaralho";
+import BaralhoInicial from "./components/BaralhoInicial";
 export default {
   name: "App",
   components: {
     CriarBaralho,
+    BaralhoInicial,
   },
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
