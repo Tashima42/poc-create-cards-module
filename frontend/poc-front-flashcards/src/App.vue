a0872b6783a19ff3ea2310d808711b5aab84bfc9
Tashima42 <tashima42@pop-os.localdomain>
2020-08-17 10:26:20 -0300

    chore(frontend): adicionado novo projeto Vue
    O projeto Vue criado anteriormente estava com as configuraçoes erradas, o novo foi criado do 0 com
    as corretas

--- frontend/poc-front-flashcards/src/App.vue
+++ frontend/poc-front-flashcards/src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <CriarBaralho />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import CriarBaralho from "./components/CriarBaralho";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    CriarBaralho,
+  },
+};
 </script>
 
 <style lang="scss">
---
+++ frontend/poc-front-flashcards/src/components/CriarBaralho.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="criar-baralho">
+    <button class="btn-criar">CRIAR BARALHO</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.btn-criar {
+  font-family: Roboto Condensed;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 36px;
+  line-height: 42px;
+  text-align: center;
+
+  background: #1f38d1;
+  color: #e8e8e8;
+}
+</style>
