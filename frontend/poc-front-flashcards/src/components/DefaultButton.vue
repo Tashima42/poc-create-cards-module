6b8e08c2fe4f26a34ef62a7ef261d7c0f7029de0
Tashima42 <tashima42@pop-os.localdomain>
2020-08-17 11:50:13 -0300

    style: added 'name' property to components
    Added the 'name' property to all componentes

--- frontend/poc-front-flashcards/src/components/DefaultButton.vue
+++ frontend/poc-front-flashcards/src/components/DefaultButton.vue
@@ -5,7 +5,9 @@
 </template>
 
 <script>
-export default {};
+export default {
+  name: "DefaultButton",
+};
 </script>
 
 <style lang="scss">
--- frontend/poc-front-flashcards/src/components/InnerDeckDisplay.vue
+++ frontend/poc-front-flashcards/src/components/InnerDeckDisplay.vue
@@ -6,6 +6,7 @@
 
 <script>
 export default {
+  name: "InnerDeckDisplay",
   data() {
     return {
       name: "baralho de testes",
