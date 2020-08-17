e3fcc9366fb58d955a355f8916e8ef1594ef9b20
Tashima42 <tashima42@pop-os.localdomain>
2020-08-17 17:38:09 -0300

    feat: added new view "New deck" and new component Insert Text Small
    Added a new component with a small text area and a new view to add new decks
    
    re: #24

---
+++ frontend/poc-front-flashcards/src/components/InsertTextSmall.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="insert-text-field">
+    <p class="text-input-label">Text label</p>
+    <input type="text" class="text-input" />
+  </div>
+</template>
+<script>
+export default {
+  name: "InsertTextSmall",
+};
+</script>
+
+<style lang="scss" scoped>
+.insert-text-field {
+  display: inline-flex;
+  flex-direction: row;
+}
+
+.text-input-label {
+  align-self: center;
+  justify-self: flex-start;
+
+  margin-right: 15px;
+
+  font-family: "Roboto Condensed", sans-serif;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 1.4rem;
+  text-align: center;
+  text-transform: uppercase;
+
+  color: #1f38d1;
+}
+
+.text-input {
+  align-self: center;
+  justify-self: flex-end;
+
+  width: 22vw;
+  min-width: 350px;
+  min-height: 37px;
+
+  margin-left: 15px;
+
+  background: #d1d8ff;
+  border: 1px solid #373737;
+  box-sizing: border-box;
+  border-radius: 20px;
+  transform: matrix(1, 0, 0, -1, 0, 0);
+}
+</style>
---
+++ frontend/poc-front-flashcards/src/views/NewDeck.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="create-deck">
+    <InsertTextSmall class="insert-text-small" />
+    <div class="create-deck-inner">
+      <InsertTextLarge class="insert-text-large" />
+      <InsertTextLarge class="insert-text-large" />
+      <DefaultButton class="add-button" buttonText="+" />
+    </div>
+    <DefaultButton class="save-button" buttonText="save" />
+  </div>
+</template>
+
+<script>
+import InsertTextSmall from "../components/InsertTextSmall";
+import InsertTextLarge from "../components/InsertTextLarge";
+import DefaultButton from "../components/DefaultButton";
+
+export default {
+  name: "NewDeck",
+  components: {
+    InsertTextSmall,
+    InsertTextLarge,
+    DefaultButton,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+$padding-size: 180px;
+
+.create-deck {
+  display: inline-flex;
+  flex-direction: column;
+}
+
+.insert-text-small {
+  align-self: center;
+}
+
+.create-deck-inner {
+  display: inline-flex;
+  flex-direction: column;
+
+  padding-left: $padding-size;
+  padding-right: $padding-size;
+  padding-top: 20px;
+  padding-bottom: 20px;
+
+  background: #d1d8ff;
+  box-shadow: 0px 12px 17px rgba(0, 0, 0, 0.14),
+    0px 5px 22px rgba(0, 0, 0, 0.12), 0px 7px 8px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+}
+
+.add-button {
+  align-self: center;
+
+  width: 60px;
+
+  margin: 20px;
+}
+
+.save-button {
+  align-self: center;
+
+  margin: 40px;
+
+  min-width: 200px;
+}
+</style>
