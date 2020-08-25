d307990fd311d5c965cdd3b0fce46666483fa4d5
Tashima42 <tashima42@pop-os.localdomain>
2020-08-25 10:09:03 -0300

    fix: imported Vuex modules to store
    Imported Vuex modules to Store
    
    re: #35

--- frontend/poc-front-flashcards/src/store/store.js
+++ frontend/poc-front-flashcards/src/store/store.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import initial from './modules/initial'
+import editDeck from './modules/editDeck'
+import newDeck from './modules/newDeck'
+import showAnswer from './modules/showAnswer'
+import showQuestion from './modules/showQuestion'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  modules: { initial, editDeck, newDeck, showAnswer, showQuestion }
 })
